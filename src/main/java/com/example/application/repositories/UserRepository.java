2e02ef20e0b55e12f698d737b6d2cefc755c5fbe
steverb1 <steverb@mindspring.com>
2025-01-29 09:42:06 -0700

    Create UserRepository.java

---
+++ src/main/java/com/example/application/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.application.repositories;
+
+import com.example.application.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
